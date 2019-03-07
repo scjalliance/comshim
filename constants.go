98700d4cefc929493dcdba60049dc7ca99f0204a
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-03-07 03:09:51 -0800

    Removed punctuation from ErrNegativeCounter

--- constants.go
+++ constants.go
@@ -6,5 +6,5 @@ var (
 	// ErrNegativeCounter is returned when the internal counter of a shim drops
 	// below zero. This may indicate that Done() has been called more than once
 	// for the same object.
-	ErrNegativeCounter = errors.New("Component object model shim counter has dropped below zero.")
+	ErrNegativeCounter = errors.New("component object model shim counter has dropped below zero")
 )
